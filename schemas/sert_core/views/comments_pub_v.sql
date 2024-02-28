6548efaeb10b50e0a8ce95e001ec36d5037485b0
Scott Spendolini <scott.spendolini@oracle.com>
2024-02-27 21:16:15 -0600

    Removed trash icon from commments report

--- schemas/sert_core/views/comments_pub_v.sql
+++ schemas/sert_core/views/comments_pub_v.sql
@@ -4,7 +4,7 @@ select
    c.comment_id
   ,replace(apex_escape.html(c.comments), chr(10), '<br />') ||
     case when c.created_by = (select v('APP_USER') from dual)
-      then '<br /><br /><span class="fa fa-trash-o"></span><a href="#" class="deleteComment" id="' || c.comment_id || '"">Delete Comment</a>'
+      then '<br /><br /><a href="#" class="deleteComment" id="' || c.comment_id || '"">Delete Comment</a>'
       else null end as comments
   ,c.rule_set_id
   ,c.rule_id
