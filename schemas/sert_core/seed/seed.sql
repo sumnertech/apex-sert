d1837851cbb3584c11d523a88cf4a0b92c49025f
Scott Spendolini <scott.spendolini@oracle.com>
2024-03-24 15:16:43 -0500

    Change rule attributes

--- schemas/sert_core/seed/seed.sql
+++ schemas/sert_core/seed/seed.sql
@@ -62,19 +62,20 @@ insert into sert_core.reserved_strings (reserved_string, reserved_string_key, re
 
 -- insert rule_criteria_types
 insert into sert_core.rule_criteria_types (rule_criteria_type_name, rule_criteria_type_key) values ('SQL Injection', 'SQLI');
-insert into sert_core.rule_criteria_types (rule_criteria_type_name, rule_criteria_type_key) values ('Cross-Site Scripting', 'XSS');
+insert into sert_core.rule_criteria_types (rule_criteria_type_name, rule_criteria_type_key) values ('Cross-Site Scripting - Item Syntax', 'XSS_ITEM_SYNTAX');
+insert into sert_core.rule_criteria_types (rule_criteria_type_name, rule_criteria_type_key) values ('Cross-Site Scripting - Unescaped htp.p', 'XSS_UNESCAPED_HTP');
 
 -- insert rule_criteria
 insert into sert_core.rule_criteria (rule_criteria_name, rule_criteria_key, rule_criteria_type_id, rule_criteria_sql, reason) values
-  ('Incorrect Item Substitution Syntax', 'INCORRECT_ITEM_SUBSTITUTION_SYNTAX', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''&[[:alnum:]]+.'', ''ix'')','Incorrect item substitution syntax');
+  ('Incorrect Item Substitution Syntax', 'INCORRECT_ITEM_SUBSTITUTION_SYNTAX_SQLI', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''&[[:alnum:]]+.'', ''ix'')','Incorrect item substitution syntax');
 insert into sert_core.rule_criteria (rule_criteria_name, rule_criteria_key, rule_criteria_type_id, rule_criteria_sql, reason) values
   ('Usage of EXECUTE IMMEDIATE', 'USAGE_OF_EXECUTE_IMMEDIATE', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''EXECUTE+[ ]+IMMEDIATE'', ''i'')','EXECUTE IMMEDIATE found; please investigate');
 insert into sert_core.rule_criteria (rule_criteria_name, rule_criteria_key, rule_criteria_type_id, rule_criteria_sql, reason) values
   ('Usage of DBMS_SQL', 'USAGE_OF_DBMS_SQL', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''dbms_sql'', ''i'')','DBMS_SQL found; please investigate');
 insert into sert_core.rule_criteria (rule_criteria_name, rule_criteria_key, rule_criteria_type_id, rule_criteria_sql, reason) values
-  ('Usage of HTP without SYS prefix', 'USAGE_OF_HTP_WITHOUT_SYS_PREFIX', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''[ ]htp.'', ''ix'')', 'Be sure to include the SYS prefix when making calls to HTP');
+  ('Usage of HTP without SYS prefix', 'USAGE_OF_HTP_WITHOUT_SYS_PREFIX', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''[ ]htp.'', ''ix'') or lower(:l_source) like ''htp.%''', 'Be sure to include the SYS prefix when making calls to HTP');
 insert into sert_core.rule_criteria (rule_criteria_name, rule_criteria_key, rule_criteria_type_id, rule_criteria_sql, reason) values
-  ('Usage of HTP without SYS prefix - First Character', 'USAGE_OF_HTP_WITHOUT_SYS_PREFIX_-_FIRST_CHARACTER', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'SQLI') , 'select count(*) from dual where lower(:l_source) like ''htp.%''', 'Be sure to include the SYS prefix when making calls to HTP');
+  ('Incorrect Item Substitution Syntax', 'INCORRECT_ITEM_SUBSTITUTION_SYNTAX_XSS', (select rule_criteria_type_id from sert_core.rule_criteria_types where rule_criteria_type_key = 'XSS_ITEM_SYNTAX'), 'select count(*) from dual where REGEXP_LIKE((:l_source), ''&[[:alnum:]]+.'', ''ix'')','Incorrect item substitution syntax');
 
 -- insert shared_comp_views
 insert into sert_core.shared_comp_views (shared_comp_view, shared_comp_type) values ('APEX_APPLICATION_ITEMS',         'Application Items');
